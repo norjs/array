f8588fa8512fd5cd7a66f8d25722442c965da74e
Jaakko-Heikki Heusala <jheusala@iki.fi>
2014-08-23 12:28:01 +0300

    Implemented optimized join()

--- src/impls/rev-object.js
+++ src/impls/rev-object.js
@@ -129,11 +129,16 @@ module.exports = function nor_array_obj(a_) {
 	 * @param from {number} The index to start the search 
 	 * @returns {number} The index of the element
 	 */
-	utils.join = function nor_array_join(s) {
-		if(arguments.length === 0) {
-			s = ',';
-		}
-		return a.join(s);
+	utils.join = function nor_array_join(s_) {
+		var t = '',
+		    s = (arguments.length === 0) ? ',' : ''+s_,
+		    l = a.length-1,
+		    i = l;
+		/*jshint plusplus:false, curly:false */
+		if(i < 0) return t;
+		t += a[l-(i--)];
+		while(i >= 0) t += s + a[l-(i--)];
+		return t;
 	};
 
 	// Export it
